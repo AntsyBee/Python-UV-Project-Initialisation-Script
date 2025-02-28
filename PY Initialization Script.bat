5fb2d52712a9592292ebc31138ae57c3c6c4b6fc
AntsyBee <sefermonty@gmail.com>
2025-02-28 15:14:50 +1100

    Improved syntax readability and added comments

--- PY Initialization Script.bat
+++ PY Initialization Script.bat
@@ -1,34 +1,35 @@
 @echo  off
 REM setting working directory to location of .bat file
 cd /d "%~dp0"
-echo set directory as: %CD%
+echo Directory set as: %cd%
 REM allows printing of current folder as project folder 
-for %%I in (%CD%) do set "ProjectFolder=%%~nxI"
+for %%i in (%cd%) do set "ProjectFolder=%%~nxI"
 
-REM setting up a confirmation dialogue box
+REM setting up a confirmation dialogue within command prompt
 echo.
-echo This script will create a PY project folder under the name %ProjectFolder% with the directory: %CD%. & echo. & echo Are you sure you wish to proceed? (Y/N):
+echo This script will initialise a PY project under the name %ProjectFolder% with the directory: %CD%. & echo. & echo Are you sure you wish to proceed? (Y/N):
 REM allows for user input under variable name confirm
 :REPROMPT
-SET /P confirm=
-IF /I "%confirm%"=="Y" GOTO RUN
-IF /I "%confirm%"=="N" GOTO ABORT
-REM in the event of misinput will reprompt 
+set /P confirm=
+REM using labels to control option selection 
+if /i "%confirm%"=="Y" goto RUN
+if /i "%confirm%"=="N" goto ABORT
+REM in the event of misinput will loop back up to reprompt label 
 echo Invalid input. Please enter Y or N.
-GOTO REPROMPT
+goto REPROMPT
 
 REM moving to desired code block using labels
 REM cancel project folder initialisation 
 :ABORT
 echo. & echo Project folder initialisation has been aborted & echo.
-GOTO END
+goto END
 
 REM confirm and initialise project folder
 :RUN
-echo initialising UV project
+echo Initialising project with UV
 uv init --no-workspace 
 
-echo making and populating .vscode directory
+echo Creating and populating .vscode directory
 mkdir .vscode
 cd .vscode 
 REM creating populated settings.json file
@@ -46,19 +47,22 @@ echo         "${workspaceFolder}/src"
 echo     ]
 echo }
 ) > settings.json
+REM creating empty extensions.json file
 type nul > extensions.json
 cd ..
 
-echo making and populating src directory
+REM creating and populating source directory 
+echo Creating and populating src directory
 mkdir src
 move hello.py src/main.py
 
-REM adding packages 
+REM package handling
 echo.
 echo If no packages are desired, leave blank. Packages can be added with uv add "packagename" at any time.
-set /P packages=Please enter desired package names (comma or space-seperated): 
+REM user input for desired packages seperated by commas or spaces. Utilizes uv add "package-name" for each package via for loop
+set /P packages=Please enter desired package names (comma or space seperated): 
 if "%packages%"=="" (
-	echo No packages slected. Skipping package installation...
+	echo No packages selected. Skipping package installation...
 ) else (
 	set packages=%packages:,= %
 	for %%p in (%packages%) do (
@@ -68,11 +72,11 @@ if "%packages%"=="" (
 		uv add %%p 
 		echo.
 	)
-echo finished installing %packages%
+echo Finished installing: %packages%
 )
-echo opening project folder in vscode
+echo Opening project folder in VSCode
 start code .
-GOTO END
+goto END
 
 REM keeps cmd open after conculsion of script operation 
 :END
